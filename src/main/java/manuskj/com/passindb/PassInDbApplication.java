d0868f9b8e28aab6d277cc520b5e419f8b83fb56
Manuela Santos <manuelass.biz@gmail.com>
2024-05-17 23:12:46 -0300

    initial commit: add base project structure

---
+++ src/main/java/manuskj/com/passindb/PassInDbApplication.java
@@ -0,0 +1,13 @@
+package manuskj.com.passindb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassInDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassInDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/manuskj/com/passindb/PassInDbApplicationTests.java
@@ -0,0 +1,13 @@
+package manuskj.com.passindb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassInDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
